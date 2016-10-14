66374d02275fc22d7aa1cc4d90782051bd680a06
Markus Frosch <markus.frosch@icinga.com>
2016-10-14 10:07:17 +0200

    IcingaObject(Legacy): Add getLegacyObjectType function
    So the type rendered to the object header can get overwritten.
    
    refs #12919

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -1893,9 +1893,14 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
         }
     }
 
+    protected function getLegacyObjectType()
+    {
+        return strtolower($this->getType());
+    }
+
     protected function renderLegacyObjectHeader()
     {
-        $type = strtolower($this->getType());
+        $type = $this->getLegacyObjectType();
 
         if ($this->isTemplate()) {
             $name = c1::renderKeyValue(
